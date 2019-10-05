2d59218d3d4d51e64f9fe84ec6c12d7ec91c1d7b
shotatsuno <number9973x@gmail.com>
2019-10-05 09:40:16 +0000

    [before start humbergermenu]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require activestorage
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
