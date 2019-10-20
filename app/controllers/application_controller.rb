ae0bba513143418258508176c37caf040f96d527
shotatsuno <number9973x@gmail.com>
2019-10-20 07:34:17 +0000

    [just before deploy]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
  end
   
  private
-    def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(resource)
     case resource
       when Admin
         new_admin_path
